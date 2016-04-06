921a477c5791ea8bafef9ca27722e704ba064b8a
Thibaud Lepretre <thibaud.lepretre@gmail.com>
2016-04-06 17:21:21 +0200

    update to Brixton.RC1

--- api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
@@ -1,13 +1,9 @@
 package com.kakawait;
 
-import com.netflix.zuul.ZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.ProxyRouteLocator;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author Thibaud Leprêtre
@@ -19,12 +15,6 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    @ConditionalOnProperty(value = "zuul.add-proxy-headers", matchIfMissing = true)
-    @Bean
-    public ZuulFilter xForwardedFilter(ProxyRouteLocator routeLocator) {
-        return new ForwardedHeaderFilter(routeLocator);
     }
 
 }
