527699ba9e5f30a5e940d8c8d50cd9b67ac3c1e8
bilak <bilaak@gmail.com>
2016-10-20 15:20:45 +0200

    test fixes -> removed deprecated SpringApplicationConfiguration and changed to SpringBootTest

--- uaa-service/src/test/java/com/kakawait/UaaServiceApplicationTests.java
+++ uaa-service/src/test/java/com/kakawait/UaaServiceApplicationTests.java
@@ -2,11 +2,11 @@ package com.kakawait;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UaaServiceApplication.class)
+@SpringBootTest(classes = UaaServiceApplication.class)
 public class UaaServiceApplicationTests {
 
 	@Test
