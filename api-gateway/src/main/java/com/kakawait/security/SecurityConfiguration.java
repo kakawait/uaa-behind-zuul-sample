7d7f2800ec4dafdd30fd797f16d0b1ae71826a4c
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2016-11-02 12:18:43 +0100

    Transform api-gateway as ResourceServer
    related to #13

--- api-gateway/src/main/java/com/kakawait/security/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/kakawait/security/SecurityConfiguration.java
@@ -1,15 +1,25 @@
 package com.kakawait.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -33,10 +43,14 @@ import java.util.regex.Pattern;
  */
 @Configuration
 @EnableOAuth2Sso
-@EnableWebSecurity
+@EnableResourceServer
+@Order(value = 0)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String CSRF_COOKIE_NAME = "XSRF-TOKEN";
     private static final String CSRF_HEADER_NAME = "X-XSRF-TOKEN";
+
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
 
     @Bean
     @Primary
@@ -51,8 +65,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .csrf().requireCsrfProtectionMatcher(csrfRequestMatcher()).csrfTokenRepository(csrfTokenRepository())
             .and()
             .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+            .addFilterAfter(oAuth2AuthenticationProcessingFilter(), AbstractPreAuthenticatedProcessingFilter.class)
             .logout().permitAll()
             .logoutSuccessUrl("/");
+    }
+
+    private OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
+        OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter =
+                new OAuth2AuthenticationProcessingFilter();
+        oAuth2AuthenticationProcessingFilter.setAuthenticationManager(oauthAuthenticationManager());
+        oAuth2AuthenticationProcessingFilter.setStateless(false);
+
+        return oAuth2AuthenticationProcessingFilter;
+    }
+
+
+    private AuthenticationManager oauthAuthenticationManager() {
+        OAuth2AuthenticationManager oauthAuthenticationManager = new OAuth2AuthenticationManager();
+        oauthAuthenticationManager.setResourceId("openid");
+        oauthAuthenticationManager.setTokenServices(tokenServices);
+        oauthAuthenticationManager.setClientDetailsService(null);
+
+        return oauthAuthenticationManager;
     }
 
     private RequestMatcher csrfRequestMatcher() {
