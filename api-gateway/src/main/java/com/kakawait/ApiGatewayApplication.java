5c94376536eee625db124e9feece016b831a67b6
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2017-03-17 16:27:22 +0100

    Remove loopback trick by Ribbon client
    Instead of using loopback trick for `accessTokenUri` like `http://localhost:${server.port}/uaa/oauth/token`, I found a way to enhance `RestTemplate` used to target `accessTokenUri` to add _load balanced_ (`Ribbon`) feature.

--- api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
@@ -2,8 +2,20 @@ package com.kakawait;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Thibaud Leprêtre
@@ -15,6 +27,18 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
+        return template -> {
+            AccessTokenProviderChain accessTokenProviderChain = Stream
+                    .of(new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
+                            new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider())
+                    .peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
+            template.setAccessTokenProvider(accessTokenProviderChain);
+        };
     }
 
 }
