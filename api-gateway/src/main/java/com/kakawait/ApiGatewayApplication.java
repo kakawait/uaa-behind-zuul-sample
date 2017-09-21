37d7422e726ae605564254de84183bac359b729e
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2017-09-21 22:08:38 +0200

    Upgrade to spring cloud dalston
    - Spring boot 1.5.7
    - Spring cloud Dalston SR3
    
    fixes #26

--- api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/kakawait/ApiGatewayApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,15 +32,14 @@ public class ApiGatewayApplication {
     }
 
     @Bean
-    UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
+    UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(LoadBalancerInterceptor loadBalancerInterceptor) {
         return template -> {
+            List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+            interceptors.add(loadBalancerInterceptor);
             AccessTokenProviderChain accessTokenProviderChain = Stream
-                    .of(
-                            new AuthorizationCodeAccessTokenProvider(),
-                            new ImplicitAccessTokenProvider(),
-                            new ResourceOwnerPasswordAccessTokenProvider(),
-                            new ClientCredentialsAccessTokenProvider())
-                    .peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+                    .of(new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
+                            new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider())
+                    .peek(tp -> tp.setInterceptors(interceptors))
                     .collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
             template.setAccessTokenProvider(accessTokenProviderChain);
         };
